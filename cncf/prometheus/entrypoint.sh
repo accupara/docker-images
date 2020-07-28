197196096c09c5bac202aa47099e844805819c2f
Uv <yuvraaj@gmail.com>
2020-07-27 21:14:41 -0700

    fix images for prometheus and grafana-loki

---
+++ cncf/prometheus/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Copyright (c) 2016-2020 Crave.io Inc. All rights reserved
+
+SRC_MOUNT=/go/src/vitess.io/vitess
+
+if [ -d $SRC_MOUNT ] ; then
+    pushd $SRC_MOUNT
+    make -C $SRC_MOUNT prep_for_docker
+    source dev.env
+    popd
+fi
+
+#set -x
+
+# Uncomment this to debug what is going on
+#echo "Running command: \"$@\""
+
+exec $@

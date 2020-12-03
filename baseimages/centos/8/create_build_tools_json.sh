9fc209b152366ad18173527a033a4ee2812e1042
Uv <yuvraaj@gmail.com>
2020-12-02 22:09:23 -0800

    Slightly clearer message at the end

--- baseimages/centos/8/create_build_tools_json.sh
+++ baseimages/centos/8/create_build_tools_json.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c90b937ea8d87d7f82ad496bef6f9826a7dcbd980e53743c28093d65f810bdf
-size 1873
+oid sha256:66612aedcf6d12c1d6c0a210cd606b39d75a17eccec7f7ff57100e61e1806e74
+size 1909

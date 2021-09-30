9a5fdc36147c58b4db9abae01e6b2629cc64ba50
Uv <uv@crave.io>
2021-09-30 00:08:15 -0700

    update windows base image

--- windows/chocobase/install.ps1
+++ windows/chocobase/install.ps1
@@ -3,4 +3,20 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+# Needed by vstools and others
 choco install -y dotnetfx
+
+# Needed by the rest of Crave
+choco install -y `
+    git `
+    make `
+    rsync `
+    vim
+
+# Add things to the PATH
+Set-ItemProperty `
+    -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' `
+    -Name PATH `
+    -Value ((Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path `
+         + "C:\Program Files\Git\bin\" `
+         + "C:\tools\vim\vim82\")

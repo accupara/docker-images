46084781e97d80c899d6bd0f5e0408a8323093db
Uv <uv@crave.io>
2021-10-16 17:15:31 -0700

    give vstools installation time to complete

--- windows/vstools/vstools2019/2019/install.ps1
+++ windows/vstools/vstools2019/2019/install.ps1
@@ -1,5 +1,7 @@
 # Remove any previous choco logs so that its easier to read logs later
 del C:\ProgramData\chocolatey\logs\chocolatey.log
 
-# Install the ENTIRE 2019 build tools. Eats up at least 55 GB.
-choco install -y visualstudio2019buildtools --package-parameters "--allWorkloads --includeRecommended --includeOptional"
+# Install the ENTIRE 2019 build tools. Eats up at least 60 GB. Takes more than an hour to complete. Give it that time and space.
+choco install -y visualstudio2019buildtools --execution-timeout 7200 --package-parameters "--allWorkloads --includeRecommended --includeOptional"
+
+Write-Output "VSTools 2019 installed"

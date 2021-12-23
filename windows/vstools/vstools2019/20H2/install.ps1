cb22c4c50e68d3c0f1cbd0c2ec41a750de649b9a
Uv <uv@crave.io>
2021-12-22 21:51:57 -0800

    fix vstools 2019 on 20H2

--- windows/vstools/vstools2019/20H2/install.ps1
+++ windows/vstools/vstools2019/20H2/install.ps1
@@ -5,4 +5,6 @@ del C:\ProgramData\chocolatey\logs\chocolatey.log
 # Install the ENTIRE 2019 build tools. Eats up at least 60 GB. Takes more than an hour to complete. Give it that time and space.
 choco install -y visualstudio2019buildtools --execution-timeout 7200 --package-parameters "--allWorkloads --includeRecommended --includeOptional"
 
+dir 'C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\'
+
 Write-Output "VSTools 2019 installed"

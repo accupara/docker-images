3a14787be9260e1a8304083900b26ab5b61349b6
Yuvraaj Kelkar <yuvraaj@gmail.com>
2022-01-03 08:58:25 -0800

    reorder commands so that the readlink works

--- tak/runme/keystore_cmds.sh
+++ tak/runme/keystore_cmds.sh
@@ -2,6 +2,9 @@
 
 set -x
 
+rm -rf atak/ATAK/app/keystore
+mkdir -p atak/ATAK/app/keystore
+
 echo 'export ANDROID_DBG_KEY_FILE=$(readlink -f atak/ATAK/app/keystore/debug.keystore)' >./scripts/exports.sh
 echo 'export ANDROID_DBG_KEY_ALIAS=androiddebugkey' >>./scripts/exports.sh
 echo 'export ANDROID_DBG_KEY_PASSWORD=android' >>./scripts/exports.sh
@@ -9,8 +12,6 @@ echo 'export ANDROID_DBG_STORE_PASSWORD=android' >>./scripts/exports.sh
 
 source ./scripts/exports.sh
 
-rm -rf atak/ATAK/app/keystore
-mkdir -p atak/ATAK/app/keystore
 keytool -genkeypair -alias ${ANDROID_DBG_KEY_ALIAS} -keypass ${ANDROID_DBG_KEY_PASSWORD} -keystore ${ANDROID_DBG_KEY_FILE} -storepass ${ANDROID_DBG_STORE_PASSWORD} -dname "CN=Android Debug,O=Android,C=US" -validity 9999
 
 # Generate local properties

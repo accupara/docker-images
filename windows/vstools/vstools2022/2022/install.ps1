1320bf306a7be3afe0b32d8a74ba5e7f049bae11
Yuvraaj Kelkar <yuvraaj@gmail.com>
2023-04-15 13:46:14 -0700

    add vs2022 for 2022

---
+++ windows/vstools/vstools2022/2022/install.ps1
@@ -0,0 +1,9 @@
+# Copyright (c) 2016-2023 Crave.io Inc. All rights reserved
+# Remove any previous choco logs so that its easier to read logs later
+del C:\ProgramData\chocolatey\logs\chocolatey.log
+
+# Install the ENTIRE 2022 build tools. Eats up at least 60 GB. Takes more than an hour to complete. Give it that time and space.
+#choco install -y visualstudio2022buildtools --execution-timeout 7200 --package-parameters "--allWorkloads --includeRecommended --includeOptional"
+choco install -y visualstudio2022community --execution-timeout 10800 --package-parameters "--allWorkloads --includeRecommended --includeOptional"
+
+Write-Output "VSTools 2022 installed"

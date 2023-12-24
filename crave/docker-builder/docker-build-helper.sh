fed4bcc547d1e02c647ca285616ae6345c330aa1
Yuvraaj Kelkar <uv@crave.io>
2023-12-24 02:47:28 -0800

    add simplifying function to the docker-build-helper.sh script

--- crave/docker-builder/docker-build-helper.sh
+++ crave/docker-builder/docker-build-helper.sh
@@ -4,11 +4,23 @@ follow_build() {
     watch "ps -ef | grep 'docker.\(build\|push\|pull\)' | grep -v runc | grep -v usr.local.lib ; echo '' ; df -h ."
 }
 
+push_finished() {
+    docker images | grep -v REPO | grep -v quay | \
+        awk '{printf("%s:%s\n", $1, $2)}' | \
+        while read line ; do
+            echo "Pushing image: $line"
+            docker push $line &
+        done
+    wait
+}
+
 main () {
     if [ "$1" == "follow" ] ; then
         follow_build
+    elif [ "$1" == "push_finished" ] ; then
+        push_finished
     else
-        echo "Usage: $0 [follow]"
+        echo "Usage: $0 [follow|push_finished]"
     fi
 }
 

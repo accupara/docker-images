8746f61bbb0cae0874fec9d6b3ee6c893fcc462f
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-03-29 23:49:24 +0530

    upload.sh: Make script nicer

--- aosp/common/upload.sh
+++ aosp/common/upload.sh
@@ -5,7 +5,7 @@ RELEASETAG=$1
 DEVICE=$2
 REPONAME=$3
 RELEASETITLE=$4
-FILES=""
+IMG_FILES=""
 
 # Check if token.txt exists
 if [ ! -f token.txt ]; then
@@ -21,22 +21,19 @@ if ! command -v gh &> /dev/null; then
     echo "gh installed."
 fi
 
-# Authenticate against github.com by reading the token from a file
+# Authenticate against github.com by reading the token from a img_file
 gh auth login --with-token < token.txt
 
-# Scan Release Files
-for file in out/target/product/$DEVICE/*.img; do
-    if [[ -n $file && $(stat -c%s "$file") -le 2147483648 ]]; then
-    FILES+="$file "
-    echo "Adding $file to FILES"
-  else
-    echo "Skipping $file"
-    continue
-  fi
+# Scan Release IMG_FILES
+for img_file in out/target/product/$DEVICE/*.img; do
+if [[ -n $img_file && $(stat -c%s "$img_file") -le 2147483648 ]]; then # Try to match github releases per size limit
+    IMG_FILES+="$img_file "
+    echo "Selecting $img_file for Upload"
+else
+    echo "Skipping $img_file"
+fi
 done
-echo "Uploaded files: $FILES"
+echo "IMG_FILES to be uploaded: $IMG_FILES"
 
 # Create release	
-gh release create $RELEASETAG out/target/product/$DEVICE/*.zip \
-                             out/target/product/$DEVICE/$FILES \
---repo $REPONAME --title $RELEASETITLE --generate-notes
+gh release create $RELEASETAG out/target/product/$DEVICE/*.zip $IMG_FILES --repo $REPONAME --title $RELEASETITLE --generate-notes

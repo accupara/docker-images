cf15b000539db4f5e5e45b35563e386d4f92af2a
Imran Khan <imran@accupara.com>
2024-05-22 11:51:51 +0400

    - add a .bash_completion file to home to actually activate bash_completion

--- crave/devspace/init.sh
+++ crave/devspace/init.sh
@@ -6,6 +6,13 @@ main() {
     mkdir -p /home/admin/.bash_completion.d
     register-python-argcomplete crave > /home/admin/.bash_completion.d/crave
     sudo chown -R admin:admin /home/admin/.bash_completion.d
+    if [ ! -f /home/admin/.bash_completion ]; then
+        cat <<EOF > /home/admin/.bash_completion
+for bcfile in ~/.bash_completion.d/* ; do
+    [ -f "\$bcfile" ] && . \$bcfile
+done
+EOF
+    fi
 }
 
 main $*

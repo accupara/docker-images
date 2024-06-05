a3db4412e453bd1ca571298d51943fbe47f4ceaa
Imran Khan <imran@accupara.com>
2024-06-06 01:22:55 +0400

    - support to autostart services from users home directory.

--- crave/devspace/startup.sh
+++ crave/devspace/startup.sh
@@ -2,5 +2,11 @@
 
 set -x
 
+if [ ! -e /home/admin/supervisor/conf.d/vscode.conf ]; then
+    cp -f /etc/crave/supervisor/examples/vscode.conf /home/admin/supervisor/conf.d/
+fi
+if [ ! -e /home/admin/supervisor/conf.d/shellinabox.conf ]; then
+    cp -f /etc/crave/supervisor/examples/shellinabox.conf /home/admin/supervisor/conf.d/
+fi
 sudo service supervisor start
 exec $*

065d099ea7e89d683fd1c2bdc70fdc0deb4dad7d
Uv <uv@crave.io>
2025-04-27 10:04:55 -0700

    replace del with Remove-Item

--- windows/vstools/vstools2019/2019/install.ps1
+++ windows/vstools/vstools2019/2019/install.ps1
@@ -1,6 +1,6 @@
 # Copyright (c) 2016-2025 Crave.io Inc. All rights reserved
 # Remove any previous choco logs so that its easier to read logs later
-del C:\ProgramData\chocolatey\logs\chocolatey.log
+Remove-Item -Force C:\ProgramData\chocolatey\logs\chocolatey.log
 
 # Install the ENTIRE 2019 build tools. Eats up at least 60 GB. Takes more than an hour to complete. Give it that time and space.
 choco install -y visualstudio2019community --execution-timeout 10800 --package-parameters "--allWorkloads --includeRecommended --includeOptional"

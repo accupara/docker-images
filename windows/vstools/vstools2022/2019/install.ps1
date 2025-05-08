3f486f396e6fafa44b2af8a5e0d08966ac9893a8
Uv <uv@crave.io>
2025-05-08 08:07:07 -0700

    test before deleting log files

--- windows/vstools/vstools2022/2019/install.ps1
+++ windows/vstools/vstools2022/2019/install.ps1
@@ -1,6 +1,8 @@
 # Copyright (c) 2016-2025 Crave.io Inc. All rights reserved
 # Remove any previous choco logs so that its easier to read logs later
-Remove-Item -Force C:\ProgramData\chocolatey\logs\chocolatey.log
+if (Test-Path C:\ProgramData\chocolatey\logs\chocolatey.log) {
+    Remove-Item C:\ProgramData\chocolatey\logs\chocolatey.log
+}
 
 # Install the ENTIRE 2022 build tools. Eats up at least 60 GB. Takes more than an hour to complete. Give it that time and space.
 #choco install -y visualstudio2022buildtools --execution-timeout 7200 --package-parameters "--allWorkloads --includeRecommended --includeOptional"

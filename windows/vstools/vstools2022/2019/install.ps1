92432643171b1ea27d76346874b2ba63bca51d76
Uv <uv@crave.io>
2025-05-15 23:03:32 -0700

    why use the silly way instead of the normal way of accepting all prompts?

--- windows/vstools/vstools2022/2019/install.ps1
+++ windows/vstools/vstools2022/2019/install.ps1
@@ -7,8 +7,8 @@ if (Test-Path C:\ProgramData\chocolatey\logs\chocolatey.log) {
 # Install the ENTIRE 2022 build tools. Eats up at least 60 GB. Takes more than an hour to complete. Give it that time and space.
 #choco install -y visualstudio2022buildtools --execution-timeout 7200 --package-parameters "--allWorkloads --includeRecommended --includeOptional"
 choco install -y visualstudio2022community --execution-timeout 10800 --package-parameters "--allWorkloads --includeRecommended --includeOptional"
+Write-Output "| VSTools 2022 installed"
 
 # Install the .NET SDK
-echo a | choco install dotnet-sdk
-
-Write-Output "VSTools 2022 installed"
+choco install -y dotnet-sdk
+Write-Output "| .NET SDK installed"

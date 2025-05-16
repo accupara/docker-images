8ada27c4beeb16c46f37d07057851090a04e1a8f
Uv <uv@crave.io>
2025-05-15 23:38:18 -0700

    vstools 2019 also gets dotnet sdk

--- windows/vstools/vstools2019/2022/install.ps1
+++ windows/vstools/vstools2019/2022/install.ps1
@@ -6,5 +6,8 @@ if (Test-Path C:\ProgramData\chocolatey\logs\chocolatey.log) {
 
 # Install the ENTIRE 2019 build tools. Eats up at least 60 GB. Takes more than an hour to complete. Give it that time and space.
 choco install -y visualstudio2019community --execution-timeout 10800 --package-parameters "--allWorkloads --includeRecommended --includeOptional"
+Write-Output "| VSTools 2022 installed"
 
-Write-Output "VSTools 2019 installed"
+# Install the .NET SDK
+choco install -y dotnet-sdk
+Write-Output "| .NET SDK installed"

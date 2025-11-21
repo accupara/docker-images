91ded5cc44dd186e5e6559aef21820fd8d3f89f0
Peace <git@z7g4n1u8.dev>
2025-11-21 12:20:02 +0530

    refactor(resync): adapt to newer version of repo and cleanup logic

--- aosp/common/resync.sh
+++ aosp/common/resync.sh
@@ -7,58 +7,36 @@ git pull -r
 cd -
 repo --version
 
+# Run repo sync command and capture the output
+find .repo -name '*.lock' -delete
+repo sync -c -j$(nproc --all) --force-sync --no-tags --no-clone-bundle --prune 2>&1 | tee /tmp/output.txt
 
-main() {
-    # Run repo sync command and capture the output
-    find .repo -name '*.lock' -delete
-    repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --prune 2>&1 | tee /tmp/output.txt
+# Check if there's any failing repositories
+if grep -qe "Failing repos\|uncommitted changes are present" /tmp/output.txt ; then
+  echo "Found failing repositories, trying to fix..."
 
- if ! grep -qe "Failing repos\|uncommitted changes are present" /tmp/output.txt ; then
-         echo "All repositories synchronized successfully."
-         exit 0
-    else
-        rm -f deleted_repositories.txt
-    fi
+  # Extract path of failing repositories
+  bad_repos=$(awk '
+    /Failing repos.*:/ {flag=1; next}
+    /Try/ {flag=0}
+    flag {print $NF}
+    /uncommitted changes are present/ {print $2}
+  ' /tmp/output.txt | sed 's/://g' | sort -u)
 
-    # Check if there are any failing repositories
-    if grep -q "Failing repos" /tmp/output.txt ; then
-        echo "Deleting failing repositories..."
-        # Extract failing repositories from the error message and echo the deletion path
-        while IFS= read -r line; do
-            # Extract repository name and path from the error message
-            repo_info=$(echo "$line" | awk -F': ' '{print $NF}')
-            repo_path=$(dirname "$repo_info")
-            repo_name=$(basename "$repo_info")
-            # Save the deletion path to a text file
-            echo "Deleted repository: $repo_info" | tee -a deleted_repositories.txt
-            # Delete the repository
-            rm -rf "$repo_path/$repo_name"
-            rm -rf ".repo/project/$repo_path/$repo_name"/*.git
-        done <<< "$(cat /tmp/output.txt | awk '/Failing repos.*:/ {flag=1; next} /Try/ {flag=0} flag' | sort -u)"
-    fi
+  # Delete all failing repositories
+  if [ -n "$bad_repos" ]; then
+    for repo_path in $bad_repos; do
+      [ -z "$repo_path" ] && continue
+      echo "Deleting: $repo_path"
+      rm -rf "$repo_path" ".repo/projects/$repo_path.git"
+    done
 
-    # Check if there are any failing repositories due to uncommitted changes
-    if grep -q "uncommitted changes are present" /tmp/output.txt ; then
-        echo "Deleting repositories with uncommitted changes..."
-
-        # Extract failing repositories from the error message and echo the deletion path
-        while IFS= read -r line; do
-            # Extract repository name and path from the error message
-            repo_info=$(echo "$line" | awk -F': ' '{print $2}')
-            repo_path=$(dirname "$repo_info")
-            repo_name=$(basename "$repo_info")
-            # Save the deletion path to a text file
-            echo "Deleted repository: $repo_info" | tee -a deleted_repositories.txt
-            # Delete the repository
-            rm -rf "$repo_path/$repo_name"
-            rm -rf ".repo/project/$repo_path/$repo_name"/*.git
-        done <<< "$(cat /tmp/output.txt | grep 'uncommitted changes are present')"
-    fi
-
-    # Re-sync all repositories after deletion
+    # Sync all repositories after deletion
     echo "Re-syncing all repositories..."
     find .repo -name '*.lock' -delete
-    repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --prune
-}
-
-main $*
+    repo sync -c -j$(nproc --all) --force-sync --no-tags --no-clone-bundle --prune
+  else
+    echo "Error detected, but could not fix. Check logs manually."
+    exit 1
+  fi
+fi
